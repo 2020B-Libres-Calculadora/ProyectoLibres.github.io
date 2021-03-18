4acc38319b9d99806b476207bd978beef379e29f
Francis Soria <59580267+kathaloz@users.noreply.github.com>
2021-03-18 15:57:43 -0500

    PoliCalcu Programa
    Versión 0.9

---
+++ PoliCalcu/src/PoliCalcu/PoliCalcuMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PoliCalcu;
+
+/**
+ *
+ * @author CARLOS
+ */
+public class PoliCalcuMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PoliCalcu.main(args);
+    }
+}
